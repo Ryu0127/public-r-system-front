950773de88a1d218b9a2ac357f4f7de6645e7f46
ryu <ryunosuke.saito0127@gmail.com>
2025-11-16 01:09:42 +0900

    レイアウト微修正

--- src/components/HashtagSearch.js
+++ src/components/HashtagSearch.js
@@ -5,7 +5,7 @@ const APP_TITLE = 'タレント別 ハッシュタグ投稿/検索 サポート'
 // タレントごとのハッシュタグデータ
 const talentHashtags = {
   tokinosora: {
-    name: 'ときのそら | Tokino Sora',
+    name: 'ときのそら（Tokino Sora）',
     hashtags: [
       { id: 1, tag: 'ときのそら', description: '一般' },
       { id: 2, tag: 'soraArt', description: 'ファンアート' },
@@ -24,7 +24,7 @@ const talentHashtags = {
     ],
   },
   roboco: {
-    name: 'ロボ子さん | Robocosan',
+    name: 'ロボ子さん（Robocosan）',
     hashtags: [
       { id: 1, tag: 'robo_co', description: '公式' },
       { id: 2, tag: 'ロボ子Art', description: 'ファンアート' },
